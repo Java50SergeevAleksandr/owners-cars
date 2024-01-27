70ab55f52007a65595fa34600521f0943c8425cd
AlGuron <blizgur8@gmail.com>
2024-01-27 23:30:34 +0200

    init

--- src/main/java/telran/cars/controller/CarsController.java
+++ src/main/java/telran/cars/controller/CarsController.java
@@ -3,6 +3,9 @@ package telran.cars.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
+import static telran.cars.api.ValidationConstants.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.RequiredArgsConstructor;
 import telran.cars.dto.*;
 import telran.cars.service.CarsService;
@@ -14,44 +17,48 @@ public class CarsController {
 	final CarsService carsService;
 
 	@PostMapping
-	CarDto addCar(@RequestBody CarDto carDto) {
-		// annotation @RequstBody informs Spring about conversion of JSON inside a
+	CarDto addCar(@RequestBody @Valid CarDto carDto) {
+		// annotation @RequestBody informs Spring about conversion of JSON inside a
 		// request to the given parameter
 		return carsService.addCar(carDto);
 	}
 
 	@PostMapping("person")
-	PersonDto addPerson(@RequestBody PersonDto personDto) {
+	PersonDto addPerson(@RequestBody @Valid PersonDto personDto) {
 		return carsService.addPerson(personDto);
 	}
 
 	@PutMapping("person")
-	PersonDto updatePerson(@RequestBody PersonDto personDto) {
+	PersonDto updatePerson(@RequestBody @Valid PersonDto personDto) {
 		return carsService.updatePerson(personDto);
 	}
 
 	@PutMapping("trade")
-	TradeDealDto purchase(@RequestBody TradeDealDto tradeDealDto) {
+	TradeDealDto purchase(@RequestBody @Valid TradeDealDto tradeDealDto) {
 		return carsService.purchase(tradeDealDto);
 	}
 
 	@DeleteMapping("person/{id}")
-	PersonDto deletePerson(@PathVariable(name = "id") long id) {
+	PersonDto deletePerson(
+			@PathVariable(name = "id") @NotNull(message = MISSING_PERSON_ID_MESSAGE) @Min(value = MIN_PERSON_ID_VALUE, message = WRONG_MIN_PERSON_ID_VALUE) @Max(value = MAX_PERSON_ID_VALUE, message = WRONG_MAX_PERSON_ID_VALUE) long id) {
 		return carsService.deletePerson(id);
 	}
 
 	@DeleteMapping("{carNumber}")
-	CarDto deleteCar(@PathVariable(name = "carNumber") String carNumber) {
+	CarDto deleteCar(
+			@PathVariable(name = "carNumber") @NotEmpty(message = MISSING_CAR_NUMBER_MESSAGE) @Pattern(regexp = CAR_NUMBER_REGEXP, message = WRONG_CAR_NUMBER_MESSAGE) String carNumber) {
 		return carsService.deleteCar(carNumber);
 	}
 
 	@GetMapping("person/{id}")
-	List<CarDto> getOwnerCars(@PathVariable(name = "id") long id) {
+	List<CarDto> getOwnerCars(
+			@PathVariable(name = "id") @NotNull(message = MISSING_PERSON_ID_MESSAGE) @Min(value = MIN_PERSON_ID_VALUE, message = WRONG_MIN_PERSON_ID_VALUE) @Max(value = MAX_PERSON_ID_VALUE, message = WRONG_MAX_PERSON_ID_VALUE) long id) {
 		return carsService.getOwnerCars(id);
 	}
 
 	@GetMapping("{carNumber}")
-	PersonDto getCarOwner(@PathVariable(name = "carNumber") String carNumber) {
+	PersonDto getCarOwner(
+			@PathVariable(name = "carNumber") @NotEmpty(message = MISSING_CAR_NUMBER_MESSAGE) @Pattern(regexp = CAR_NUMBER_REGEXP, message = WRONG_CAR_NUMBER_MESSAGE) String carNumber) {
 		return carsService.getCarOwner(carNumber);
 	}
 }
--- src/main/java/telran/cars/exceptions/controller/CarsExceptionsController.java
+++ src/main/java/telran/cars/exceptions/controller/CarsExceptionsController.java
@@ -1,9 +1,13 @@
 package telran.cars.exceptions.controller;
 
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 import telran.cars.exceptions.NotFoundException;
 
@@ -17,5 +21,19 @@ public class CarsExceptionsController {
 	@ExceptionHandler(IllegalStateException.class)
 	ResponseEntity<String> illegalStateHandler(IllegalStateException e) {
 		return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	ResponseEntity<String> methodArgumentNotValidHandler(MethodArgumentNotValidException e) {
+		String message = e.getAllErrors().stream().map(error -> error.getDefaultMessage())
+				.collect(Collectors.joining(";"));
+		return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(HandlerMethodValidationException.class)
+	ResponseEntity<String> methodValidationHandler(HandlerMethodValidationException e) {
+		String message = e.getAllErrors().stream().map(error -> error.getDefaultMessage())
+				.collect(Collectors.joining(";"));	
+		return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
 	}
 }
